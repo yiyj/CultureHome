6ebf1aa1a9f869dac00ae388392eb392663c87e3
yyj <609354782@qq.com>
2017-10-02 19:00:54 +0800

    Initial Commit

---
+++ CultureHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CultureHome
+//
+//  Created by apple on 2017/10/2.
+//  Copyright © 2017年 易元江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CultureHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CultureHome
+//
+//  Created by apple on 2017/10/2.
+//  Copyright © 2017年 易元江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
